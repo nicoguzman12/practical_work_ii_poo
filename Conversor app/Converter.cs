f73e3320fa90ede7fbaf00a2256c3c146f00d2ec
nicoguzman12 <9403538@alumnos.ufv.es>
2025-05-21 11:50:08 +0200

    removing console interactions from converter.cs

--- Conversor app/Converter.cs
+++ Conversor app/Converter.cs
@@ -13,12 +13,17 @@ namespace Conversor_app
 
             this.operations.Add(new DecimalToBinary("Decimal to Binary", "Decimal to Binary"));
             this.operations.Add(new DecimalToOctal("Decimal to Octal", "Decimal to Octal"));
-            this.operations.Add(new DecimalToTwoComplement("Decimal to Two's Complement", "Decimal to Two's Complement")); 
+            this.operations.Add(new DecimalToTwoComplement("Decimal to Two's Complement", "Decimal to Two's Complement"));
             this.operations.Add(new DecimalToHexadecimal("Decimal to Hexadecimal", "Decimal to Hexadecimal"));
             this.operations.Add(new TwoComplementToDecimal("Two's Complement to Decimal", "Binary (TwoComplement) to Decimal"));
             this.operations.Add(new BinaryToDecimal("Binary to Decimal", "Binary to Decimal"));
             this.operations.Add(new OctalToDecimal("Octal to Decimal", "Octal to Decimal"));
             this.operations.Add(new HexadecimalToDecimal("Hexadecimal to Decimal", "Hexadecimal to Decimal"));
+        }
+
+        public Conversion GetOperation(int index)
+        {
+            return operations[index];
         }
 
         public int Exit()
@@ -31,28 +36,7 @@ namespace Conversor_app
             return this.operations.Count;
         }
 
-        public int PrintOperations()
-        {
-            Console.Clear();
-
-            Console.WriteLine("---------------------------------------");
-
-            for (int i = 1; i <= this.operations.Count; i++)
-            {
-                Console.WriteLine($"{i}. {this.operations[i - 1].GetName()}");
-            }
-
-            Console.WriteLine($"{this.Exit()}. Exit");
-            Console.Write("---------------------------------------");
-
-            string tmp = Console.ReadLine();
-
-            if (tmp == "") return this.Exit();
-
-            int option = int.Parse(tmp);
-
-            return (option <= 1 || option > this.GetNumberOfOperations()) ? this.Exit() + 1 : option;
-        }
+        //PrintOperations methods are removed
 
         public string PerformConversion(int op, string input)
         {
@@ -66,11 +50,6 @@ namespace Conversor_app
             }
 
             return this.operations[op - 1].Change(input);
-        }
-
-        public void PrintConversion(int op, string input, string output)
-        {
-            this.operations[op - 1].PrintConversion(input, output);
         }
     }
 }
